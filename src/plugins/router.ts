1d9441417ff6001e95fefbbf41fe3c5082b2c32e
kuromiii <dominator56600@gmail.com>
2025-05-20 22:59:51 +0200

    Test router fix

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -4,8 +4,8 @@ import HomeView from '../views/HomeView.vue'
 import TableEditorView from '../views/TableEditorView.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/tableeditor', component: TableEditorView },
+  { path: '', component: HomeView },
+  { path: 'tableeditor', component: TableEditorView },
 ]
 
 export const router = createRouter({
