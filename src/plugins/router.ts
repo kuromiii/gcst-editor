937b34d0fa82d7fcf382b90173f6cbfbb0833c5b
kuromiii <dominator56600@gmail.com>
2025-05-20 23:02:50 +0200

    Test router fix

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -4,11 +4,11 @@ import HomeView from '../views/HomeView.vue'
 import TableEditorView from '../views/TableEditorView.vue'
 
 const routes = [
-  { path: '', component: HomeView },
-  { path: 'tableeditor', component: TableEditorView },
+  { path: '/', component: HomeView },
+  { path: '/tableeditor', component: TableEditorView },
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/gcst-editor/'),
   routes,
 })
