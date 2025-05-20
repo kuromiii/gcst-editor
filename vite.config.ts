f423de141076d1531f024f8eda80c0c419851ae0
kuromiii <dominator56600@gmail.com>
2025-05-20 22:55:15 +0200

    Added missing base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gcst-editor/',
   plugins: [vue()],
   // Fix Bootstrap SCSS warnings
   css: {
